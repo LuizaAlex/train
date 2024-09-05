e07768638bde1903637696f3391a3480dda8e13c
LuizaAlex <poteca.luiza@gmail.com>
2024-09-05 22:08:13 +0300

    .

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -2,7 +2,12 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
@@ -11,17 +16,42 @@ import java.util.Map;
 @LambdaHandler(
     lambdaName = "sqs_handler",
 	roleName = "sqs_handler-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
+	isPublishVersion = false,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
+@SqsTriggerEventSource(
+	targetQueue = "async_queue",
+	batchSize = 1
+)
+
+@DependsOn(
+	name = "async_queue",
+	resourceType = ResourceType.SQS_QUEUE
+)
+public class SqsHandler implements RequestHandler<SQSEvent, Void> {
+
+    @Override
+    public Void handleRequest(SQSEvent sqsEvent, Context context) {
+       
+		for (SQSEvent.SQSMessage msg : sqsEvent.getRecords()){
+			processMessage(msg, context);
+
+		}
+
+		context.getLogger().log("done");
+		return null;
 	}
+
+		private void processMessage(SQSEvent.SQSMessage msg, Context context){
+			try{
+				context.getLogger().log("Processed message " +  msg.getBody());
+
+			}catch(Exception e ){
+				context.getLogger().log("Error processing message " + msg.getBody());
+				throw e;
+			}
+		}
+
+    
 }
